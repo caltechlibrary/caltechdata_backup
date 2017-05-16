0246a34ad958b2900be2dd94dd3fd684c99c9c2c
Tom Morrell <tmorrell@caltech.edu>
2017-05-16 10:33:32 -0700

    Added multi-page support

--- caltechdata_read.py
+++ caltechdata_read.py
@@ -7,11 +7,37 @@ import json
 import os
 import shutil
 
+def read_records(data):
+    #read records in 'hits' structure
+    for record in data:
+        outstr = json.dumps(record)
+
+        #Replace single quotes with complicated escape
+        outstr = outstr.replace("'","'\\''")
+        print(str(record['id']))
+        os.system("echo '" + outstr +"' | dataset create "+str(record['id'])+'.json')
+        
+        #Download all file
+        metadata = record['metadata']
+        if 'electronic_location_and_access' in metadata:
+            for erecord in  metadata['electronic_location_and_access']:
+                url = erecord["uniform_resource_identifier"]
+                #req = urllib.request.Request(url)
+                #s = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+                #response = urllib.request.urlopen(req,context=s)
+
+                #outfile = open(erecord['electronic_name'][0],'wb')
+                #outfile.write(response.read())
+                print(erecord['electronic_name'][0])    
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=\
     "caltechdata_read queries the caltechDATA (Invenio 3) API\
     returns data and adds to dataset structure on disk")
+
+    os.environ["DATASET"] = "caltechdata"
+
     #parser.add_argument('-t', dest='topic_id',help="topic of questions to\
     #        return (default all)")
 
@@ -33,27 +59,11 @@ if __name__ == "__main__":
     response = urllib.request.urlopen(req,context=s)
     data = json.JSONDecoder().decode(response.read().decode('UTF-8'))
 
-    for f in data['hits']['hits']:
-        #o = open(str(f['id'])+'.json','w')
-        #if f['files']:
-        #    print(f['files'])
-        outstr = json.dumps(f)
-        #print(outstr)
+    read_records(data['hits']['hits'])
+    #if we have more pages of data
+    while 'next' in data['links']:
+        req = urllib.request.Request(data['links']['next'])        
+        response = urllib.request.urlopen(req,context=s)
+        data = json.JSONDecoder().decode(response.read().decode('UTF-8'))
         
-        #Replace single quotes with complicated escape
-        outstr = outstr.replace("'","'\\''")
-        print(str(f['id']))
-        os.system("echo '" + outstr +"' | dataset create "+str(f['id'])+'1.json')  
-
-        #Download all file
-        record = f['metadata']
-        if 'electronic_location_and_access' in record:
-            for erecord in  record['electronic_location_and_access']:
-                url = erecord["uniform_resource_identifier"]
-                req = urllib.request.Request(url)
-                s = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-                response = urllib.request.urlopen(req,context=s)
-            
-                outfile = open(erecord['electronic_name'][0],'wb')
-                outfile.write(response.read())
-                print(erecord['electronic_name'][0])
+        read_records(data['hits']['hits'])
