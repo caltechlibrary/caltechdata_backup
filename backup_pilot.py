c430a411d5191247857101b6c5b9d81581e731d9
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-07-13 14:03:13 -0700

    Support one directory lower file checks

--- backup_pilot.py
+++ backup_pilot.py
@@ -1,4 +1,4 @@
-import argparse, os, json
+import argparse, os, json, time
 import s3fs, boto3, glob
 from progressbar import ProgressBar
 from datacite import schema43
@@ -24,7 +24,10 @@ def upload_file(f, file, size, bucket, path, s3_boto):
     print(file)
     bar = ProgressBar(max_value=size)
     s3_boto.upload_fileobj(f, bucket, f"{file}", Callback=Progress(bar))
-    assert size == s3.du(f"{bucket}/{file}")
+    time.sleep(10)
+    uploaded_size = s3.du(f"{bucket}/{file}")
+    print(uploaded_size)
+    assert size == uploaded_size
 
 
 parser = argparse.ArgumentParser(
@@ -48,6 +51,10 @@ current = s3.ls(f"{bucket}/{path}{folder}")
 existing = []
 for ex in current:
     existing.append(ex.split(f"{bucket}/")[1])
+    #We support looking at one level of folders
+    second_level = s3.ls(ex)
+    for sec in second_level:
+        existing.append(sec.split(f"{bucket}/")[1])
 
 # Now use boto version of backup location to ensure copying works
 s3_boto = boto3.client("s3")
@@ -55,34 +62,37 @@ s3_boto = boto3.client("s3")
 if location == "local":
     backup_source = glob.glob(f"{folder}/*")
     for file in backup_source:
-        if file not in existing:
-            if os.path.isfile(file):
+        if os.path.isfile(file):
+            if file not in existing:
                 size = os.path.getsize(file)
                 print(file)
                 with open(file, "rb") as f:
                     upload_file(f, f"{path}{file}", size, bucket, path, s3_boto)
-            else:
-                for fil in glob.glob(f"{file}/*"):
+        else:
+            for fil in glob.glob(f"{file}/*"):
+                if fil not in existing:
                     print(fil)
                     size = os.path.getsize(fil)
                     with open(fil, "rb") as f:
                         upload_file(f, f"{path}{fil}", size, bucket, path, s3_boto)
+
 elif location == "OSN":
     endpoint = "https://renc.osn.xsede.org/"
     osn_s3 = s3fs.S3FileSystem(anon=True, client_kwargs={"endpoint_url": endpoint})
     # Find the files to backup
     backup_source = osn_s3.glob(f"{path}{folder}/*")
     for file in backup_source:
-        if file not in existing:
-            size = osn_s3.info(file)["Size"]
-            if size > 0:
-                # we have a file
+        size = osn_s3.info(file)["Size"]
+        if size > 0:
+            #we have a fille
+            if file not in existing:
                 print(file)
                 with osn_s3.open(file, "rb") as f:
                     upload_file(f, file, size, bucket, path, s3_boto)
-            else:
-                # We have a directory, get all the files under it
-                for fil in osn_s3.glob(f"{file}/*"):
+        else:
+            # We have a directory, get all the files under it
+            for fil in osn_s3.glob(f"{file}/*"):
+                if fil not in existing:    
                     print(fil)
                     size = osn_s3.info(fil)["Size"]
                     with osn_s3.open(fil, "rb") as f:
